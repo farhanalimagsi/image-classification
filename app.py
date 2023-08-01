2eff5785456cde16bc8fa0bfe8d386afd60a62b9
Farhan Ali <131546625+farhanalimagsi@users.noreply.github.com>
2023-07-31 22:04:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ from PIL import Image
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-@st.cache_resource()
-def load_model():
-
-    model = tf.keras.models.load_model('model.h5')
-    return model
+#@st.cache_resource()
+#def load_model():
+   # model = tf.keras.models.load_model('model.h5')
+   # return model
 @st.cache_resource()
 def load_model_dog_breed():
     model_dog_breed = tf.keras.models.load_model('dog_breed.h5',
@@ -19,12 +18,12 @@ def load_model_dog_breed():
     return model_dog_breed
 
 
-def predict_class(image, model):
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, [224, 224])
-    image = tf.expand_dims(image, axis=0)
-    prediction = model.predict(image)
-    return prediction
+#def predict_class(image, model):
+   # image = tf.cast(image, tf.float32)
+    #image = tf.image.resize(image, [224, 224])
+    #image = tf.expand_dims(image, axis=0)
+    #prediction = model.predict(image)
+    #return prediction
 
 def predict_dog_class(image, model):
     imag = tf.cast(image, tf.float32)
@@ -37,35 +36,35 @@ def predict_dog_class(image, model):
 st.title("Multi-Class Image Classification")
 st.sidebar.title("Select the Image Class")
 
-app_mode = st.sidebar.radio("Pick image class", ["Food","Dog Breed"])
-if app_mode == 'Food':  
-    model = load_model()
+app_mode = st.sidebar.radio("Pick image class", ["Dog Breed","Food"])
 
 if app_mode == 'Dog Breed':
     dog_breed_model = load_model_dog_breed()
+#if app_mode == 'Food':  
+    #model = load_model()
 
-if app_mode == 'Food':
+#if app_mode == 'Food':
 
-    file = st.file_uploader("Upload an image of food", type=["jpg", "jpeg", "png"])
-    col1, col2 = st.columns(2)
+    #file = st.file_uploader("Upload an image of food", type=["jpg", "jpeg", "png"])
+   # col1, col2 = st.columns(2)
 
-    col1.subheader("Food Image Prediction")
-    col2.subheader("Result")
+    #col1.subheader("Food Image Prediction")
+    #col2.subheader("Result")
 
-    if file is None:
-        st.text("Waiting for upload....")
-    else: 
-        test_image = Image.open(file)
-        slot = st.empty()
-        col1.image(test_image, caption="Input Image", width=300)
-        slot.text('Running inference.....')
-        pred = tf.squeeze(predict_class(test_image, model))
-        class_names = pd.read_csv("class_names.txt")
-        class_names = np.array(class_names["labels"])
-        result = class_names[tf.argmax(pred)]
-        slot.text('Done')
-        output = 'The image is a ' + result
-        col2.success(output)
+    #if file is None:
+        #st.text("Waiting for upload....")
+    #else: 
+        #test_image = Image.open(file)
+        #slot = st.empty()
+        #col1.image(test_image, caption="Input Image", width=300)
+        #slot.text('Running inference.....')
+        #pred = tf.squeeze(predict_class(test_image, model))
+        #class_names = pd.read_csv("class_names.txt")
+        #class_names = np.array(class_names["labels"])
+        #result = class_names[tf.argmax(pred)]
+        #slot.text('Done')
+        #output = 'The image is a ' + result
+        #col2.success(output)
 
 
 if app_mode == 'Dog Breed':
